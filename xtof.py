2eabebbd02d516f635b35973ab7df4b64b9ab4d5
Christophe Cerisara <cerisara@loria.fr>
2022-01-29 16:59:19 +0100

    works well on religion

--- xtof.py
+++ xtof.py
@@ -2,6 +2,7 @@ DATADIR = "/home/xtof/nas1/TALC/Synalp/Corpus/MMUL/data/test/"
 files = ("world_religions_test.csv",)
 
 import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 def format_example(df, idx, include_answer=True):
     prompt = df.iloc[idx, 0]
@@ -17,10 +18,31 @@ test_df = pd.read_csv(DATADIR+files[0], header=None)
 answers = choices[:test_df.shape[1]-2]
 print(test_df.shape)
 print(answers)
+
+tokenizer = AutoTokenizer.from_pretrained("allenai/unifiedqa-t5-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("allenai/unifiedqa-t5-large")
+
+nok,nrep,ntot=0,0,0
 for i in range(test_df.shape[0]):
     prompt_end = format_example(test_df, i, include_answer=False)
     label = test_df.iloc[i, test_df.shape[1]-1]
     print()
-    print(label)
+    gold = ord(label)-ord('A')
+    print(label,gold)
     print(prompt_end)
+    s = tokenizer.encode(prompt_end,return_tensors='pt')
+    y = model.generate(s)
+    sy = tokenizer.decode(y[0])
+    sy = sy.replace('<pad>','').replace("</s>","").strip()
+    opts = prompt_end.split('\n')
+    rep=-1
+    for j,s in enumerate(opts):
+        if sy in s: rep=ord(s[1])-ord('a')
+    ntot+=1
+    if rep>=0:
+        nrep+=1
+        if gold==rep: nok+=1
+    acc=float(nok)/float(ntot)
+    ans=float(nrep)/float(ntot)
+    print(sy,"ACC=",acc,ans,rep)
 
